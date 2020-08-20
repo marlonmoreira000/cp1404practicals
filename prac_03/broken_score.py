6458a6e19708c6b6ba6417162549ca0206e04df4
Marlon Moreira <marlonmoreira000@gmail.com>
2020-08-20 16:35:59 +1000

    Modularise broken_score.py using functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,20 @@
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+def main():
+    """ Rate university scores """
+
+    score = float(input("Enter score: "))
+    categorise_score(score)
+
+
+def categorise_score(score):
+    """ Categorise scores from 0 to 100 """
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score < 50:
+        print("Bad")
+    elif score < 90:
+        print("Passable")
+    else:
+        print("Excellent")
+
+
+main()
