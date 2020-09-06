20ceb7bfda15ccffbde3d87d34e4c931e71fc492
Marlon Moreira <marlonmoreira000@gmail.com>
2020-09-06 18:47:21 +1000

    Finish list_exercises.py exercise

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,15 @@
+"""This program takes in 5 numbers and puts them in a list.
+It then prints various properties of this set of numbers"""
+
+number_of_numbers = 5
+numbers = []
+
+for i in range(1, number_of_numbers + 1):
+    number = int(input("Number: "))
+    numbers.append(number)
+
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of numbers is {}".format(sum(numbers) / len(numbers)))
