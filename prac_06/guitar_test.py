938362e47f2694aca0a84b0f6ed3fb370b009bc1
Marlon Moreira <marlonmoreira000@gmail.com>
2020-09-12 16:51:56 +1000

    reformat guitar_test.py code

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,6 +1,7 @@
 """This program is used to do simple tests on the Guitar class we created"""
 from prac_06.guitar import Guitar
 
+
 def main():
     # create 2 Guitar classes. Class(name, year, cost)
     guitar1 = Guitar("Gibson 1000", 1937, 13500)
@@ -11,3 +12,6 @@ def main():
     print("{} get_age() - Expected 19. Got {}".format(guitar2.name, guitar2.get_age()))
     print("{} is_vintage - Expected True. Got {}".format(guitar1.name, guitar1.is_vintage()))
     print("{} is_vintage - Expected True. Got {}".format(guitar2.name, guitar2.is_vintage()))
+
+
+main()
