f4482242a2f16db84ede809254bc6df30a65b946
Marlon Moreira <marlonmoreira000@gmail.com>
2020-09-12 15:19:30 +1000

    finish languages.py exercise for learning classes

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,14 +1,18 @@
 """Simple program to test ProgrammingLanguage class"""
-
 from prac_06.programming_language import ProgrammingLanguage
 
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-languages = [ruby, python, visual_basic]
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-print("The dynamically typed languages are:")
-for language in languages:
-    if language.is_dynamic() == True:
-        print(language.name)
+    languages = [ruby, python, visual_basic]
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic() == True:
+            print(language.name)
+
+
+main()
