4831cac0140cab03c1227ed47c1ce01fe1855679
Marlon Moreira <marlonmoreira000@gmail.com>
2020-11-02 14:50:19 +1000

    Finish sort_files_1.py exercise

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,24 @@
+"""
+Sort files by their extension name
+"""
+import os
+
+
+def main():
+    """Move files into folders with the same name as their extension."""
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+        # Ignore directories
+        if os.path.isdir(filename):
+            continue
+
+        extension = filename.split('.')[-1]
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        print("{}/{}".format(extension, filename))
+        os.rename(filename, "{}/{}".format(extension, filename))
+
+
+main()
